f0c4ecd5d6c24fdbd0ea3940d17fad833f078635
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-06 20:01:48 +0300

    updated my views

---
+++ gallery/urls.py
@@ -0,0 +1,13 @@
+from django.urls import url
+from .import views
+
+
+urlpatterns=[
+
+    url(r'^$',views.gallery, name ='gallery'),
+    url(r'^$',views.gallery, name ='gallery'),
+    url(r'^$',views.gallery, name ='gallery')
+
+
+
+]
--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def gallery(request):
+    return render(request,'my-gallery/gallery.html')
+
+def viewPhoto(request):
+    return render(request,'my-gallery/photos.html')
+
+
+def addPhoto(request):
+    return render(request,'my-gallery/form.html')
