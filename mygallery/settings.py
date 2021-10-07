ac54ed08802aa8bd50711795f378b0a1447f2837
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-07 13:47:04 +0300

    modified settings .py and uploaded an image

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -121,10 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/img/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    os.path.join(BASE_DIR,"static"),
 ]
-MEDIA_ROOT = BASE_DIR /  'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
