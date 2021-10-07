2e144cc48257bc48e3788bf9293f229036602c3b
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-07 13:31:20 +0300

    modiefied settings.py file

--- mygallery/settings.py
+++ mygallery/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/img/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+MEDIA_ROOT = BASE_DIR /  'static/images'
