5336af98ab28250bbd1188dd4837b3b498fa4f42
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-10-09 02:23:57 +0300

    added a delete in urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -9,7 +9,8 @@ urlpatterns=[
     path('',views.gallery, name ='gallery'),
     path('photo/<str:pk>/',views.viewPhoto, name ='photo'),
     path('add/',views.addPhoto, name ='add'),
-    path('search_results/',views.search_results, name='search_results')
+    path('search_results/',views.search_results, name='search_results'),
+    path('delete_event/<event_id>',views.delete_event,name='delete-event'),
 
 ]
 
